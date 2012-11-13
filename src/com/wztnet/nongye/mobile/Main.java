8a9ad3ed606e917ad527581e035d0bf2fbb34531
wennercn <wenner.cn@gmail.com>
2012-11-13 17:27:53 +0800

    修改1

---
+++ src/com/wztnet/nongye/mobile/Main.java
@@ -0,0 +1,16 @@
+package com.wztnet.nongye.mobile;
+
+//import android.app.Activity;
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+
+public class Main extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);  
+    	super.setIntegerProperty("splashscreen", R.drawable.splash);
+        super.loadUrl("file:///android_asset/www/index.html", 4500);
+    }
+}
