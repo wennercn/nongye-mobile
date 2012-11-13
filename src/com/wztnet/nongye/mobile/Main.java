a853b5dc98b6f141e61b1d5a2f1e5fa9da455044
wennercn <wenner.cn@gmail.com>
2012-11-13 17:58:46 +0800

    测试

--- src/com/wztnet/nongye/mobile/Main.java
+++ src/com/wztnet/nongye/mobile/Main.java
@@ -11,6 +11,6 @@ public class Main extends DroidGap {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);  
     	super.setIntegerProperty("splashscreen", R.drawable.splash);
-        super.loadUrl("file:///android_asset/www/index.html", 4500);
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 }
